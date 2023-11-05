9e0725a0e767e3d18fb1eab0188ca21b4dceb289
Patrick Portes <89034086+PatrickPortes@users.noreply.github.com>
2023-11-05 12:20:41 -0300

    MainActivity e ViewBinding
    Criando e inserindo a logo do projeto e iniciando o desenvolvimento das views do layout principal (logo, botão, textviews), e desenvolvendo a viewbinding da MainActivity com o layout principal do projeto.

--- app/src/main/java/com/example/currencyquoteapp/MainActivity.kt
+++ app/src/main/java/com/example/currencyquoteapp/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.currencyquoteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.currencyquoteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //Atributos:
+
+    //viewBinding:
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var btRecuperar: Button
+    private lateinit var tvBitcoin: TextView
+    private lateinit var tvEthereum: TextView
+
+    //Métodos:
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //viewBinding:
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        startingComponents()
+
+        btRecuperar.setOnClickListener {
+
+            //Recuperar Dados da Api
+            TODO()
+        }
+    }
+
+    private fun startingComponents() {
+        btRecuperar = binding.btRecuperar
+        tvBitcoin = binding.tvBitcoin
+        tvEthereum = binding.tvEthereum
     }
 }
